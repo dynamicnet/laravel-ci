9b24f5e35e73d1775da7c467357b8f65c712458f
David TOUREL <david@kauriweb.com>
2021-08-18 17:54:43 +0200

    Fix bad concatenation for SSH config generation + better sync for storage/ directory

--- scripts/build-deploy-script.php
+++ scripts/build-deploy-script.php
@@ -1,3 +1,4 @@
+#!/usr/local/bin/php
 <?php
 
 /**
@@ -13,13 +14,15 @@
  */
 
 
+echo "#!/bin/sh\n";
 
 if (! isset($_ENV["DEPLOY_PRIVATE_KEY"])) {
     echo "exit 0\n";
     exit(0);
 }
 
-echo 'echo -e \"Host {$_ENV["DEPLOY_TARGET_HOST"]}\n\tStrictHostKeyChecking no\n\tUser {$_ENV["DEPLOY_USER"]}\n\n\" > ~/.ssh/config'."\n";
+echo "mkdir -p ~/.ssh/\n";
+echo 'echo -e "Host '.$_ENV["DEPLOY_TARGET_HOST"].'\n\tStrictHostKeyChecking no\n\tUser '.$_ENV["DEPLOY_USER"].'\n\n" > ~/.ssh/config'."\n";
 echo 'echo -e "'.$_ENV["DEPLOY_PRIVATE_KEY"].'" > ~/.ssh/id_rsa'."\n";
 echo 'chmod 0400 ~/.ssh/id_rsa'."\n";
 echo 'ssh-add ~/.ssh/id_rsa'."\n";
@@ -29,10 +32,14 @@ echo 'ssh-add ~/.ssh/id_rsa'."\n";
 // on exclus donc "storage" pour ne pas supprimer tous les fichiers sur l'env de destination
 echo "rsync -auz -e \"ssh\" --delete --exclude 'storage/*' --exclude '.git' ./ {$_ENV["DEPLOY_USER"]}@{$_ENV["DEPLOY_TARGET_HOST"]}:{$_ENV["DEPLOY_DIR"]}\n";
 
+// Synchro de storage sans le flag --delete
+echo "rsync -auz -e \"ssh\" ./storage/ {$_ENV["DEPLOY_USER"]}@{$_ENV["DEPLOY_TARGET_HOST"]}:{$_ENV["DEPLOY_DIR"]}/storage\n";
+
 if (! isset($_ENV["DEPLOY_CHOWN_USER"])) {
     echo "ssh -ttq {$_ENV["DEPLOY_USER"]}@{$_ENV["DEPLOY_TARGET_HOST"]} \"chown -R {$_ENV["DEPLOY_CHOWN_USER"]}:{$_ENV["DEPLOY_CHOWN_GROUP"]} {$_ENV["DEPLOY_DIR"]}\"\n";
 }
 
+echo "ssh -ttq {$_ENV["DEPLOY_USER"]}@{$_ENV["DEPLOY_TARGET_HOST"]} \"{$_ENV["DEPLOY_HOST_PHP_PATH"]} {$_ENV["DEPLOY_ARTISAN_PATH"]} key:generate\"\n";
 echo "ssh -ttq {$_ENV["DEPLOY_USER"]}@{$_ENV["DEPLOY_TARGET_HOST"]} \"{$_ENV["DEPLOY_HOST_PHP_PATH"]} {$_ENV["DEPLOY_ARTISAN_PATH"]} migrate\"\n";
 
 #ssh -ttq root@192.168.0.60 "docker exec PHP74 php /var/www/html/p/pierre-fabre.dev/library/www/artisan route:cache"
