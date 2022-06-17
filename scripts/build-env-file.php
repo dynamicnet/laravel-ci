49a9dc2ba9db7a3ee4a3f25907aa43079c32f0c6
David TOUREL <david@kauriweb.com>
2022-06-17 10:45:51 +0200

    tri alphabetique des var d'env > .env

--- scripts/build-env-file.php
+++ scripts/build-env-file.php
@@ -1,19 +1,31 @@
 #!/usr/local/bin/php
 <?php
 
+$extracted_env = [
+    "APP_KEY" => "" // Set a placeholder for the app key generated later by Laravel's artisan command
+];
+
+// Extrait les variables d'environnement qui doivent être inscrites dans le .env
+// Elles sont préfixées par DOTENV_
 foreach ($_ENV as $name => $value) {
     $prefix_position = strpos($name, "DOTENV_");
 
-    if (false === $prefix_position || $prefix_position > 0 ) {
+    if (false === $prefix_position || $prefix_position > 0) {
         continue;
     }
 
+    // Strip prefix
+    $name = str_replace('DOTENV_', '', $name);
+
+    $extracted_env[$name] = $value;
+}
+
+// Sort by keys for a better readability of the resulting .env file
+ksort($fruits);
+
+foreach ($extracted_env as $name => $value) {
     // escape double quote in value
     $value = str_replace('"', '\"', $value);
-    $name = str_replace('DOTENV_', '', $name);
 
     echo "{$name}=\"{$value}\"\n";
 }
-
-// Set a placeholder for the app key generated later with artisan
-echo "APP_KEY=\n";
